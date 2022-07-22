1e03b459dadd9e116456442f525c0862a27e1c6d
anacarol.costa <carol.costa2502@gmail.com>
2022-07-22 18:48:40 -0300

    criar projeto com spring initializr

---
+++ src/main/kotlin/com/anacarolcosta/apidorama/ApiDoramaApplication.kt
@@ -0,0 +1,11 @@
+package com.anacarolcosta.apidorama
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiDoramaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiDoramaApplication>(*args)
+}
---
+++ src/test/kotlin/com/anacarolcosta/apidorama/ApiDoramaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anacarolcosta.apidorama
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiDoramaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
