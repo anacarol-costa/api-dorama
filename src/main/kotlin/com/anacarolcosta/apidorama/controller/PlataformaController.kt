070c133cc716ce9624082fc94b23dc1ac48b0c61
anacarol.costa <carol.costa2502@gmail.com>
2022-08-17 15:27:51 -0300

    mudar o retorno de post plataforma para created (201)

--- src/main/kotlin/com/anacarolcosta/apidorama/controller/PlataformaController.kt
+++ src/main/kotlin/com/anacarolcosta/apidorama/controller/PlataformaController.kt
@@ -2,10 +2,12 @@ package com.anacarolcosta.apidorama.controller
 
 import com.anacarolcosta.apidorama.controller.request.PostPlataformaRequest
 import com.anacarolcosta.apidorama.model.PlataformaModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -18,6 +20,7 @@ class PlataformaController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody plataforma: PostPlataformaRequest) {
         println(plataforma)
     }
