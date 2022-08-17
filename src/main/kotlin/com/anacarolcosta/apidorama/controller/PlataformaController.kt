806adb4ed5fb83e78d38cf1c2b123cfa5cdeb9a8
anacarol.costa <carol.costa2502@gmail.com>
2022-08-17 15:54:52 -0300

    implementar get por id de plataforma

--- src/main/kotlin/com/anacarolcosta/apidorama/controller/PlataformaController.kt
+++ src/main/kotlin/com/anacarolcosta/apidorama/controller/PlataformaController.kt
@@ -4,6 +4,7 @@ import com.anacarolcosta.apidorama.controller.request.PostPlataformaRequest
 import com.anacarolcosta.apidorama.model.PlataformaModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,12 +15,18 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("plataformas")
 class PlataformaController {
 
-    val plataformas = mutableListOf<PlataformaModel>()
+    private val plataformas = mutableListOf<PlataformaModel>()
 
     @GetMapping
-    fun getPlataforma(): MutableList<PlataformaModel> {
+    fun getAllPlataforma(): MutableList<PlataformaModel> {
         return plataformas
     }
+
+    @GetMapping("/{id}")
+    fun getPlataforma(@PathVariable id: String): PlataformaModel {
+        return plataformas.filter { it.id == id }.first()
+    }
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
