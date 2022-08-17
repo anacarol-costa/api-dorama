fb7a6ee277338119ad35ce4b56eaf5be221189ad
anacarol.costa <carol.costa2502@gmail.com>
2022-08-17 15:24:13 -0300

    implementar post plataforma

--- src/main/kotlin/com/anacarolcosta/apidorama/controller/PlataformaController.kt
+++ src/main/kotlin/com/anacarolcosta/apidorama/controller/PlataformaController.kt
@@ -1,7 +1,10 @@
 package com.anacarolcosta.apidorama.controller
 
+import com.anacarolcosta.apidorama.controller.request.PostPlataformaRequest
 import com.anacarolcosta.apidorama.model.PlataformaModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,5 +15,10 @@ class PlataformaController {
     @GetMapping
     fun getPlataforma(): PlataformaModel {
         return PlataformaModel("1", "Netflix")
+    }
+
+    @PostMapping
+    fun create(@RequestBody plataforma: PostPlataformaRequest) {
+        println(plataforma)
     }
 }
---
+++ src/main/kotlin/com/anacarolcosta/apidorama/controller/request/PostPlataformaRequest.kt
@@ -0,0 +1,5 @@
+package com.anacarolcosta.apidorama.controller.request
+
+data class PostPlataformaRequest (
+    var nomePlataforma: String
+)
